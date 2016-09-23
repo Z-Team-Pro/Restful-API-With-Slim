07a7bda1de7ead20c14a70c676a1057473e31169
Z-Team-Pro <pencilsoft1@gmail.com>
2016-09-23 17:30:56 +0200

    lesson9 Send JSON response

--- Public/index.php
+++ Public/index.php
@@ -34,6 +34,19 @@ $app->get('/testargs/{name}/{phone}',function($request ,$response,$args){
 $name=$args['name'];
 $phone=$args['phone'];
 $response->getBody()->write("This is a test for args ,$name your phone number is $phone");
+});
+//Json Response TEST 
+$app->get('/jsontest/{FirstName}/{LastName}',function($Request ,$Response ,$args){
+
+$FirstName=$args['FirstName'];
+$LastName=$args['LastName'];
+$out=[];
+$out['Status']=200;
+$out['Message']="This is JSON Response TEST";
+$out['FirstName']=$FirstName;
+$out['LastName']=$LastName;
+$Response->getBody()->write(json_encode($out));
+
 }); 
 $app->run();
 //
