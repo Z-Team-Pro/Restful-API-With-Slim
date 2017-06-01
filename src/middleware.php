9466a81fc9d4bb900df5a105ada4ffce37cd3376
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-02 01:12:07 +0200

    JWT Validation

--- src/middleware.php
+++ src/middleware.php
@@ -36,6 +36,20 @@ $app->add( new \Slim\Middleware\HttpBasicAuthentication(
 ));
 
 
+$app->add(new \Slim\Middleware\JwtAuthentication([
+	 "path" => ["/"],
+    "passthrough" => ["/JWTToken"],
+    "secret" => "supersecretkeyyoushouldnotcommittogithub",
+    "error" => function ($request, $response, $arguments) {
+        $data["status"] = "error";
+        $data["message"] = $arguments["message"];
+        return $response
+            ->withHeader("Content-Type", "application/json")
+            ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    }
+]));
+
+
 
 
 
