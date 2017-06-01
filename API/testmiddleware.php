b95d7d406cd17253da8920f5b78ae3320f71f043
Ramy Hakam <ramyhakam2@gmail.com>
2017-06-01 22:39:41 +0200

    middleware 4

--- API/testmiddleware.php
+++ API/testmiddleware.php
@@ -36,3 +36,31 @@ $app->get('/withMidClass',function($req,$res){
 $res->getBody()->write("---this is My Route With middleware Class ----");
 
 })->add(new middelwareClass());
+
+
+//test for add attributes from middleware 
+
+
+$addit=function($req,$res,$next){
+
+$res->getBody()->write("---before adding attribute ----");
+$req=$req->withAttribute('passed',true);
+$res=$next($req,$res);
+$res->getBody()->write("---after adding attribute ----");
+
+return $res;
+
+};
+
+//route wite more aattribute
+$app->get('/testAddingAttribute',function($req,$res){
+
+
+$myparm=$req->getAttribute('passed');
+
+$message=array("middleware_attribute"=>$myparm);
+
+$res->withJson($message,200);
+
+
+})->add($addit);
